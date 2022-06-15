abee031adb6090018677aff7a30f3d2ed357c8a3
Zoran Nikolov <zoran902@gmail.com>
2022-06-15 15:42:50 +0300

    Update

---
+++ JavaScript-Fundamentals/3. Arrays/1. Arrays - Lab/solve.js
@@ -0,0 +1,9 @@
+function solve() {
+  let arr = [1, 2, 3, 4, 5];
+
+  console.log(arr);
+  console.log(arr.includes(50));
+  console.log(arr.includes(5));
+  console.log(arr.join("*"));
+}
+solve();
