d2e7150352c4e0892c938ab432fa08adfa085530
Zoran Nikolov <zoran902@gmail.com>
2022-07-06 21:18:22 +0300

    Update

--- JavaScript-Fundamentals/5. Arrays Advanced/2. Arrays Advanced - Exercise/08.arrayManipulator.js
+++ JavaScript-Fundamentals/5. Arrays Advanced/2. Arrays Advanced - Exercise/08.arrayManipulator.js
@@ -12,15 +12,19 @@ function arrayManipulator(nums, commands) {
         nums.splice(indexToAdd, 0, elementToAdd);
         break;
       case "addMany":
-        let indexToAddMany = currentValues.shift();
-        let elementToAddMany = currentValues.join(", ");
-        nums.splice(indexToAddMany, 0, elementToAddMany);
+        let indexToAddMany = Number(currentValues.shift());
+        let elementToAddMany = currentValues;
+        let currentElementToAddMany = 0;
+        for (let i = elementToAddMany.length - 1; i >= 0; i--) {
+          currentElementToAddMany = Number(elementToAddMany[i]);
+          nums.splice(indexToAddMany, 0, currentElementToAddMany);
+        }
         break;
       case "contains":
         console.log(nums.indexOf(Number(currentValues[0])));
         break;
       case "remove":
-        let indexToRemove = currentValues[0];
+        let indexToRemove = Number(currentValues[0]);
         nums.splice(indexToRemove, 1);
         break;
       case "shift":
@@ -44,12 +48,7 @@ function arrayManipulator(nums, commands) {
     }
   }
 }
-// arrayManipulator(
-//   [1, 2, 4, 5, 6, 7],
-//   ["add 1 8", "contains 1", "contains 3", "print"]
-// );
-// arrayManipulator(
-//   [1, 2, 4, 5, 6, 7],
-//   ["add 1 8", "contains 1", "contains 3", "print"]
-// );
-arrayManipulator([2, 2, 4, 2, 4], ["add 1 4", "sumPairs", "print"]);
+arrayManipulator(
+  [1, 2, 3, 4, 5],
+  ["addMany 5 9 8 7 6 5", "contains 15", "remove 3", "shift 1", "print"]
+);
