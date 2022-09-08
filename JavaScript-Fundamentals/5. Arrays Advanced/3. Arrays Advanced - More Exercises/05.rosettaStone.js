b9f4bac869ea3efb3856d7d18972527100802987
Zoran Nikolov <zoran902@gmail.com>
2022-09-08 17:00:02 +0300

    update

--- JavaScript-Fundamentals/5. Arrays Advanced/3. Arrays Advanced - More Exercises/05.rosettaStone.js
+++ JavaScript-Fundamentals/5. Arrays Advanced/3. Arrays Advanced - More Exercises/05.rosettaStone.js
@@ -1,27 +1,22 @@
 function rosettaStone(array) {
   let matrix = array.slice();
   let templateLength = Number(matrix.shift());
-  let templateMatrix = matrix
+
+  let tempMatrix = matrix
     .splice(0, templateLength)
     .map((row) => row.split(" ").map((x) => Number(x)));
-  matrix = matrix.map((row) => row.split(" ").map((x) => Number(x)));
 
-  for (let matrixRow = 0; matrixRow < matrix.length; matrixRow += templateLength) {
-    for (let matrixCol = 0; matrixCol < matrix.length; matrixCol += templateMatrix[0].length) {
-        
-      for (let templateMatrixRow = 0; templateMatrixRow < templateLength; templateMatrixRow++) {
-        for (let templateMatrixCol = 0;templateMatrixCol < templateMatrix[0].length;templateMatrixCol++) {
-            
-          let code = 0;
-          if (matrixCol + templateMatrixCol > matrix.length - 1 || matrixRow + templateMatrixRow > matrix.length - 1) {
-            continue;
-          } else {
-            code = matrix[matrixRow + templateMatrixRow][matrixCol + templateMatrixCol] + templateMatrix[templateMatrixRow][templateMatrixCol];
-            let currentSymbol = getLetter(code);
-            matrix[matrixRow + templateMatrixRow].splice(matrixCol + templateMatrixCol, 1,currentSymbol);
-          }
-        }
-      }
+  matrix = matrix.map((row) => row.split(" ").map((x) => Number(x)));
+  let result = "";
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      // current index position
+      let code = matrix[row][col];
+      // template overlay
+      let templateDecoder =
+        tempMatrix[row % templateLength][col % tempMatrix[0].length];
+      let currentSymbol = code + templateDecoder;
+      result += getLetter(currentSymbol);
     }
   }
 
@@ -34,18 +29,10 @@ function rosettaStone(array) {
     if (num > 0) {
       letter = String.fromCharCode(num + 64);
     }
-      
     return letter;
   }
 
-  let res = [];
-  matrix
-    .map((el) => {
-      res.push(el.join(""));
-    })
-    .join("");
-
-  console.log(res.join("").trim());
+  console.log(result);
 }
 rosettaStone([
   "2",
