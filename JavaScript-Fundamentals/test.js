978e9efa705a34cd592784ce7db2955ccf97a587
Zoran Nikolov <zoran902@gmail.com>
2022-10-11 17:13:14 +0300

    updt

--- JavaScript-Fundamentals/test.js
+++ JavaScript-Fundamentals/test.js
@@ -1,49 +1,18 @@
-function vacation(input) {
-  let index = 0;
-  let excursionPrice = Number(input[index++]);
-  let money = Number(input[index++]);
-  let action = "";
-  let currentMoney = 0;
-  let daySpent = 0;
-  let dayCounter = 0;
+function minNumber(input) {
+    let index = 0;
+    let num = input[index];
+    index++;
+    let minNum = Number.MAX_SAFE_INTEGER;
 
-  while (money < excursionPrice) {
-    action = input[index++];
-    currentMoney = Number(input[index++]);
-    dayCounter++;
-    switch (action) {
-      case "spend":
-        money -= currentMoney;
-        if (money < currentMoney) {
-          money = 0;
+    while (num !== "Stop") {
+        let currentNum = Number(num);
+
+        if (currentNum < minNum) {
+            minNum = currentNum;
         }
-        daySpent++;
-        break;
-      case "save":
-        money += currentMoney;
-        daySpent = 0;
-        break;
+        num = input[index];
+        index++;
     }
-    if (daySpent >= 5) {
-      console.log(`You can't save the money.`);
-      console.log(`${dayCounter}`);
-      break;
-    }
-  }
-  if (money >= excursionPrice) {
-    console.log(`You saved the money for ${dayCounter} days.`);
-  }
+    console.log(minNum);
 }
-vacation(["150",
-  "150",
-  "spend",
-  "50",
-  "spend",
-  "50",
-  "save",
-  "100",
-  "save",
-  "100"])
-
-
-
+minNumber(["100", "99", "80", "70", "Stop"]);
