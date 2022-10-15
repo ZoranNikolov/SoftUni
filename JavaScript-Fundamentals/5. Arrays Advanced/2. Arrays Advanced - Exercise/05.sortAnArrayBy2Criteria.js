baec60fcc6ddd3031af56ab1fd0e6ac781d56d57
Zoran Nikolov <zoran902@gmail.com>
2022-10-15 12:15:17 +0300

    Update 05.sortAnArrayBy2Criteria.js

--- JavaScript-Fundamentals/5. Arrays Advanced/2. Arrays Advanced - Exercise/05.sortAnArrayBy2Criteria.js
+++ JavaScript-Fundamentals/5. Arrays Advanced/2. Arrays Advanced - Exercise/05.sortAnArrayBy2Criteria.js
@@ -1,11 +1,9 @@
 function sortAnArrayBy2Criteria(arr) {
-  let sortedArray = arr.sort((a, b) => {
-    return a.length - b.length || a.localeCompare(b);
-  });
+	let sortedArray = arr.sort((a, b) => {
+		return a.length - b.length || a.localeCompare(b);
+	});
 
-  sortedArray.forEach((element) => {
-    console.log(element);
-  });
+	sortedArray.forEach(element => console.log(element));
 }
 sortAnArrayBy2Criteria(["alpha", "beta", "gamma"]);
 // sortAnArrayBy2Criteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
