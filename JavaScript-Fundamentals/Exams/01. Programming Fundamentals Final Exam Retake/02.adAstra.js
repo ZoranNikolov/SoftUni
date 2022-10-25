b51a5ae38182b1effd28bcefc69bb6b3155460c6
Zoran Nikolov <zoran902@gmail.com>
2022-10-25 16:04:00 +0300

    updt

--- JavaScript-Fundamentals/Exams/01. Programming Fundamentals Final Exam Retake/02.adAstra.js
+++ JavaScript-Fundamentals/Exams/01. Programming Fundamentals Final Exam Retake/02.adAstra.js
@@ -1,9 +1,31 @@
 function adAstra(input) {
+	let pattern =
+		/([\||#])(?<item>[a-zA-Z\s]+)\1(?<date>\d{2}\/\d{2}\/\d{2})\1(?<calories>[\d+]{1,5})\1/g;
+	let match = pattern.exec(input);
+	let totalCalories = 0;
+	let foodInfo = [];
 
-    
+	while (match !== null) {
+		foodInfo.push({
+			item: match.groups.item,
+			date: match.groups.date,
+			calories: match.groups.calories,
+		});
+		totalCalories += Number(match.groups.calories);
+		match = pattern.exec(input);
+	}
 
+	let timeToLast = Math.floor(totalCalories / 2000);
+	console.log(`You have food to last you for: ${timeToLast} days!`);
+
+	for (const el of foodInfo) {
+		console.log(`Item: ${el.item}, Best before: ${el.date}, Nutrition: ${el.calories}`);
+	}
 }
+adAstra([
+	"#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|",
+]);
 adAstra([
 	"$$#@@%^&#Fish#24/12/20#8500#|#Incorrect#19.03.20#450|$5*(@!#Ice Cream#03/10/21#9000#^#@aswe|Milk|05/09/20|2000|",
 ]);
-// adAstra(["Hello|#Invalid food#19/03/20#450|$5*(@"]);
+adAstra(["Hello|#Invalid food#19/03/20#450|$5*(@"]);
---
+++ JavaScript-Fundamentals/Exams/01. Programming Fundamentals Final Exam Retake/03.thePianist.js
@@ -0,0 +1,154 @@
+function thePianist(input) {
+	let piecesCount = Number(input.shift());
+	let store = {};
+
+	for (let i = 0; i < piecesCount; i++) {
+		let [piece, composer, tone] = input.shift().split("|");
+		if (!store.hasOwnProperty(piece)) {
+			store[piece] = {};
+			store[piece][composer] = tone;
+		}
+	}
+
+	let commandParser = {
+		Add: (store, pieceToAdd, composerToAdd, toneToAdd) => {
+			if (!store.hasOwnProperty(pieceToAdd)) {
+				store[pieceToAdd] = {};
+				store[pieceToAdd][composerToAdd] = toneToAdd;
+				console.log(
+					`${pieceToAdd} by ${composerToAdd} in ${toneToAdd} added to the collection!`
+				);
+			} else {
+				console.log(`${pieceToAdd} is already in the collection!`);
+			}
+			return store;
+		},
+		Remove: (store, pieceToRemove) => {
+			if (store.hasOwnProperty(pieceToRemove)) {
+				console.log(`Successfully removed ${pieceToRemove}!`);
+				delete store[pieceToRemove];
+			} else {
+				console.log(
+					`Invalid operation! ${pieceToRemove} does not exist in the collection.`
+				);
+			}
+			return store;
+		},
+		ChangeKey: (store, pieceChange, newTone) => {
+			if (store.hasOwnProperty(pieceChange)) {
+				for (const [key, value] of Object.entries(store[pieceChange])) {
+					store[pieceChange][key] = newTone;
+				}
+				console.log(`Changed the key of ${pieceChange} to ${newTone}!`);
+			} else {
+				console.log(`Invalid operation! ${pieceChange} does not exist in the collection.`);
+			}
+			return store;
+		},
+	};
+
+	input.forEach((line) => {
+		let [command, ...tokens] = line.split("|");
+		if (command !== "Stop") {
+			store = commandParser[command](store, ...tokens);
+		}
+	});
+	for (const [piece, komposerTone] of Object.entries(store)) {
+		for (const [composer, tone] of Object.entries(komposerTone)) {
+			console.log(`${piece} -> Composer: ${composer}, Key: ${tone}`);
+		}
+	}
+}
+thePianist([
+	"3",
+	"Fur Elise|Beethoven|A Minor",
+	"Moonlight Sonata|Beethoven|C# Minor",
+	"Clair de Lune|Debussy|C# Minor",
+	"Add|Sonata No.2|Chopin|B Minor",
+	"Add|Hungarian Rhapsody No.2|Liszt|C# Minor",
+	"Add|Fur Elise|Beethoven|C# Minor",
+	"Remove|Clair de Lune",
+	"ChangeKey|Moonlight Sonata|C# Major",
+	"Stop",
+]);
+
+/*
+function thePianist(input) {
+	let piecesCount = Number(input.shift());
+	let store = {};
+
+	for (let i = 0; i < piecesCount; i++) {
+		let [piece, composer, tone] = input.shift().split("|");
+		if (!store.hasOwnProperty(piece)) {
+			store[piece] = {};
+			store[piece][composer] = tone;
+		}
+	}
+	for (const line of input) {
+		let [command, ...tokens] = line.split("|");
+		if (command === "Stop") {
+			break;
+		}
+
+		switch (command) {
+			case "Add":
+				let pieceToAdd = tokens[0];
+				if (!store.hasOwnProperty(pieceToAdd)) {
+					let composerToAdd = tokens[1];
+					let toneToAdd = tokens[2];
+					store[pieceToAdd] = {};
+					store[pieceToAdd][composerToAdd] = toneToAdd;
+					console.log(
+						`${pieceToAdd} by ${composerToAdd} in ${toneToAdd} added to the collection!`
+					);
+				} else {
+					console.log(`${pieceToAdd} is already in the collection!`);
+				}
+				break;
+			case "Remove":
+				let pieceToRemove = tokens[0];
+				if (store.hasOwnProperty(pieceToRemove)) {
+					delete store[pieceToRemove];
+					console.log(`Successfully removed ${pieceToRemove}!`);
+				} else {
+					console.log(
+						`Invalid operation! ${pieceToRemove} does not exist in the collection.`
+					);
+				}
+				break;
+			case "ChangeKey":
+				let pieceChange = tokens[0];
+				if (store.hasOwnProperty(pieceChange)) {
+					let newTone = tokens[1];
+					for (const [key, value] of Object.entries(store[pieceChange])) {
+						store[pieceChange][key] = newTone;
+					}
+					console.log(`Changed the key of ${pieceChange} to ${newTone}!`);
+				} else {
+					console.log(
+						`Invalid operation! ${pieceChange} does not exist in the collection.`
+					);
+				}
+				break;
+		}
+	}
+	for (const [piece, komposerTone] of Object.entries(store)) {
+		for (const [composer, tone] of Object.entries(komposerTone)) {
+			console.log(`${piece} -> Composer: ${composer}, Key: ${tone}`);
+		}
+	}
+}
+thePianist([
+	"3",
+	"Fur Elise|Beethoven|A Minor",
+	"Moonlight Sonata|Beethoven|C# Minor",
+	"Clair de Lune|Debussy|C# Minor",
+	"Add|Sonata No.2|Chopin|B Minor",
+	"Add|Hungarian Rhapsody No.2|Liszt|C# Minor",
+	"Add|Fur Elise|Beethoven|C# Minor",
+	"Remove|Clair de Lune",
+	"ChangeKey|Moonlight Sonata|C# Major",
+	"Stop",
+]);
+
+*/
