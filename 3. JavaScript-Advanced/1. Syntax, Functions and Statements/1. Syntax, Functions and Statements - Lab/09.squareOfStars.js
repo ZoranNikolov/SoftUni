4f26e87fd83a199c0fcfdd628cb4a91f8981acb6
Zoran Nikolov <zoran902@gmail.com>
2022-11-03 17:33:19 +0200

    updt

--- 3. JavaScript-Advanced/1. Syntax, Functions and Statements/1. Syntax, Functions and Statements - Lab/09.squareOfStars.js
+++ 3. JavaScript-Advanced/1. Syntax, Functions and Statements/1. Syntax, Functions and Statements - Lab/09.squareOfStars.js
@@ -1,3 +1,14 @@
-function squareOfStars(params) {
-	
+function squareOfStars(size) {
+	let arr = [];
+
+	for (let col = 0; col < size; col++) {
+		arr[col] = "*";
+		for (let row = 0; row < size; row++) {
+			arr.push([row], [col]);
+		}
+	}
+	console.log(arr);
 }
+// squareOfStars(1);
+squareOfStars(5);
+// squareOfStars(7);
