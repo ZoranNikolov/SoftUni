5266f9957c76728bc031aa488404db2715eab336
Zoran Nikolov <zoran902@gmail.com>
2022-12-04 10:33:01 +0200

    updt

--- 2. JavaScript-Fundamentals/Exams/Final Exam Preparation/03.piratesPrep.js
+++ 2. JavaScript-Fundamentals/Exams/Final Exam Preparation/03.piratesPrep.js
@@ -20,7 +20,27 @@ function pirates(input) {
 		Plunder(cities, city, people, gold) {
 			people = Number(people);
 			gold = Number(gold);
-			
+			cities[city].population -= people;
+			cities[city].gold -= gold;
+			console.log(
+				`${city} plundered! ${gold} gold stolen, ${people} citizens killed.`
+			);
+			if (cities[city].population === 0 || cities[city].gold === 0) {
+				delete cities[city];
+				console.log(`${city} has been wiped off the map!`);
+			}
+			return cities;
+		},
+		Prosper(cities, city, gold) {
+			gold = Number(gold);
+			if (gold >= 0) {
+				cities[city].gold += gold;
+				console.log(
+					`${gold} gold added to the city treasury. ${city} now has ${cities[city].gold} gold.`
+				);
+			} else {
+				console.log("Gold added cannot be a negative number!");
+			}
 			return cities;
 		},
 	};
@@ -29,15 +49,38 @@ function pirates(input) {
 		if (line === "End") {
 			break;
 		}
+
+		let [command, ...tokens] = line.split("=>");
+		cities = commandParser[command](cities, ...tokens);
+	}
+
+	if (Object.entries(cities).length > 0) {
+		console.log(
+			`Ahoy, Captain! There are ${
+				Object.entries(cities).length
+			} wealthy settlements to go to:`
+		);
+		for (const city in cities) {
+			console.log(
+				`${city} -> Population: ${cities[city].population} citizens, Gold: ${cities[city].gold} kg`
+			);
+		}
+	} else {
+		console.log(
+			"Ahoy, Captain! All targets have been plundered and destroyed!"
+		);
 	}
 }
-
 pirates([
-	"Tortuga||345000||1250",
-	"Santo Domingo||240000||630",
-	"Havana||410000||1100",
+	"Nassau||95000||1000",
+	"San Juan||930000||1250",
+	"Campeche||270000||690",
+	"Port Royal||320000||1000",
+	"Port Royal||100000||2000",
 	"Sail",
-	"Plunder=>Tortuga=>75000=>380",
-	"Prosper=>Santo Domingo=>180",
+	"Prosper=>Port Royal=>-200",
+	"Plunder=>Nassau=>94000=>750",
+	"Plunder=>Nassau=>1000=>150",
+	"Plunder=>Campeche=>150000=>690",
 	"End",
 ]);
