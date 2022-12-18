004342a4b91c43db248917a2cb09c9da894fc65b
Zoran Nikolov <zoran902@gmail.com>
2022-12-18 16:46:39 +0200

    updt

---
+++ 3. JavaScript-Advanced/2. Arrays and Nested Arrays/1. Arrays and Nested Arrays - Lab/11.equalNeighbors.js
@@ -0,0 +1,15 @@
+function equalNeoghbors(matrix) {
+	let pairCounter = 0;
+
+	for (let row = 0; row < matrix.length; row++) {
+		for (let col = 0; col < matrix[row].length; col++) {
+
+		}
+	}
+	console.log(pairCounter);
+}
+equalNeoghbors([
+	["test", "yes", "yo", "ho"],
+	["well", "done", "yo", "6"],
+	["not", "done", "yet", "5"],
+]);
