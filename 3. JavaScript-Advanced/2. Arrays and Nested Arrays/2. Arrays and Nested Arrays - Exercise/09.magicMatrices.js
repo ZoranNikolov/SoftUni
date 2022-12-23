4f45c11abbe3b0fa597e37d7ffe1a098f708d27d
Zoran Nikolov <zoran902@gmail.com>
2022-12-23 11:36:31 +0200

    updt

--- 3. JavaScript-Advanced/2. Arrays and Nested Arrays/2. Arrays and Nested Arrays - Exercise/09.magicMatrices.js
+++ 3. JavaScript-Advanced/2. Arrays and Nested Arrays/2. Arrays and Nested Arrays - Exercise/09.magicMatrices.js
@@ -1,8 +1,23 @@
 function magicMatrices(matrix) {
-	
+	for (let i = 0; i < matrix.length - 1; i++) {
+		let sumRowOne = matrix[i].reduce((acc, el) => acc + el);
+		let sumRowTwo = matrix[i + 1].reduce((acc, el) => acc + el);
+		let sumColOne = 0;
+		let sumColTwo = 0;
+		for (let col = 0; col < matrix.length; col++) {
+			sumColOne += matrix[i][col];
+			sumColTwo += matrix[i + 1][col];
+		}
+		if (sumRowOne !== sumRowTwo || sumColOne !== sumColTwo) {
+			return false;
+		}
+	}
+	return true;
 }
-magicMatrices([
-	[4, 5, 6],
-	[6, 5, 4],
-	[5, 5, 5],
-]);
+console.log(
+	magicMatrices([
+		[4, 5, 6],
+		[6, 5, 4],
+		[5, 5, 5],
+	])
+);
