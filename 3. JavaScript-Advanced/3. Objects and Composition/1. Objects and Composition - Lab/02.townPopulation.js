1ff73807e3978ac37b1c879e4242da6a4c3d2241
Zoran Nikolov <zoran902@gmail.com>
2023-01-04 22:41:10 +0200

    updt

--- 3. JavaScript-Advanced/3. Objects and Composition/1. Objects and Composition - Lab/02.townPopulation.js
+++ 3. JavaScript-Advanced/3. Objects and Composition/1. Objects and Composition - Lab/02.townPopulation.js
@@ -1,11 +1,29 @@
-function townPopulation(array) {
-	let result = {}
-return townData = 100
+function townPopulation(townData) {
+	let townInfoObj = townData
+		.map((line) => {
+			let data = line.split(" <-> ");
+			return {
+				name: data[0],
+				population: Number(data[1]),
+			};
+		})
+		.reduce((result, town) => {
+			if (!result.hasOwnProperty(town.name)) {
+				result[town.name] = town.population;
+			} else {
+				result[town.name] += town.population;
+			}
+			return result;
+		}, {});
+
+	for (const key in townInfoObj) {
+		console.log(`${key} : ${townInfoObj[key]}`);
+	}
 }
-console.log(townPopulation([
+townPopulation([
 	"Sofia <-> 1200000",
 	"Montana <-> 20000",
 	"New York <-> 10000000",
 	"Washington <-> 2345000",
 	"Las Vegas <-> 1000000",
-]))
+]);
