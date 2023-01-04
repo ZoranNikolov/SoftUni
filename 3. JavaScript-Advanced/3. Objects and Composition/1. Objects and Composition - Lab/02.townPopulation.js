a3829ef323d41d979b7404cd29d012fdf9833a54
Zoran Nikolov <zoran902@gmail.com>
2023-01-04 22:13:17 +0200

    updt

---
+++ 3. JavaScript-Advanced/3. Objects and Composition/1. Objects and Composition - Lab/02.townPopulation.js
@@ -0,0 +1,11 @@
+function townPopulation(array) {
+	let result = {}
+return townData = 100
+}
+console.log(townPopulation([
+	"Sofia <-> 1200000",
+	"Montana <-> 20000",
+	"New York <-> 10000000",
+	"Washington <-> 2345000",
+	"Las Vegas <-> 1000000",
+]))
--- 3. JavaScript-Advanced/3. Objects and Composition/1. Objects and Composition - Lab/06.fromJsonToHtmlTable.js
+++ 3. JavaScript-Advanced/3. Objects and Composition/1. Objects and Composition - Lab/06.fromJsonToHtmlTable.js
@@ -10,11 +10,11 @@ function jsonToHtmlTable(json) {
 	result += "</table>";
 
 	function appendValues(values) {
-		for (let value of values) {
+		for (let currentValue of values) {
 			result += `    <tr>`;
-			result += `<td>${escape(value[0])}</td><td>${escape(
-				value[1]
-			)}</td>`;
+			for (const value of currentValue) {
+				result += `<td>${escape(value)}</td>`;
+			}
 			result += "</tr>\n";
 		}
 	}
@@ -28,11 +28,26 @@ function jsonToHtmlTable(json) {
 	}
 
 	function escape(value) {
-		return value.toString().replace("<", "&lt;").replace(">", "&gt;");
+		return value
+			.toString()
+			.replace(/&/g, "&amp;")
+			.replace(/</g, "&lt;")
+			.replace(/>/g, "&gt;")
+			.replace(/"/g, "&quot;")
+			.replace(/'/g, "&#39;");
 	}
 	console.log(result);
 }
-jsonToHtmlTable(`[{"Name":"Stamat",
-"Score":5.5},
-{"Name":"Rumen",
-"Score":6}]`);
+// jsonToHtmlTable(`[{"Name":"Stamat",
+// "Score":5.5},
+// {"Name":"Rumen",
+// "Score":6}]`);
+jsonToHtmlTable(`[{"Name":"Pesho",
+"Score":4,
+"Grade":8},
+{"Name":"Gosho",
+"Score":5,
+"Grade":8},
+{"Name":"Angel",
+"Score":5.50,
+"Grade":10}]`);
