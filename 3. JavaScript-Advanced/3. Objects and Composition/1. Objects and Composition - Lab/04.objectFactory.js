48f6951efaa977ec6daa1f72cdae8ae063b6e907
Zoran Nikolov <zoran902@gmail.com>
2023-01-05 13:35:31 +0200

    updt

---
+++ 3. JavaScript-Advanced/3. Objects and Composition/1. Objects and Composition - Lab/04.objectFactory.js
@@ -0,0 +1,32 @@
+function factory(library, orders) {
+	const result = [];
+
+	for (const order of orders) {
+		let current = Object.assign({}, order.template);
+		for (const part of order.parts) {
+			current[part] = library[part];
+		}
+		result.push(current);
+	}
+	return result;
+}
+
+const library = {
+	print: function () {
+		console.log(`${this.name} is printing a page`);
+	},
+	scan: function () {
+		console.log(`${this.name} is scanning a document`);
+	},
+	play: function (artist, track) {
+		console.log(`${this.name} is playing '${track}' by ${artist}`);
+	},
+};
+const orders = [
+	{ template: { name: "ACME Printer" }, parts: ["print"] },
+	{ template: { name: "Initech Scanner" }, parts: ["scan"] },
+	{ template: { name: "ComTron Copier" }, parts: ["scan", "print"] },
+	{ template: { name: "BoomBox Stereo" }, parts: ["play"] },
+];
+const products = factory(library, orders);
+console.log(products);
