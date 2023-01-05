313b5d8379879ce36c1a04e98d4dc058ca9aef00
Zoran Nikolov <zoran902@gmail.com>
2023-01-05 17:00:59 +0200

    updt

--- 3. JavaScript-Advanced/3. Objects and Composition/2. Objects and Composition - Exercise/11.jansNotation.js
+++ 3. JavaScript-Advanced/3. Objects and Composition/2. Objects and Composition - Exercise/11.jansNotation.js
@@ -1,6 +1,30 @@
-function jansNotation(params) {
+function jansNotation(arr) {
+	res = [];
+	let numbers = [];
+	let operators = [];
 
-	
+	for (const el of arr) {
+		if (typeof el == "number") {
+			numbers.push(el);
+		} else {
+			operators.push(el);
+		}
+	}
 
+	if (operators.length < numbers.length - 1) {
+		console.log("Error: too many operands!");
+		return;
+	} else if (operators.length === numbers.length || numbers.length === 0) {
+		console.log("Error: not enough operands!");
+		return;
+	}
+
+	for (const el of arr) {
+		if (typeof el == "number") {
+			res.push(el);
+			continue;
+		}
+		let numA = res.pop();
+	}
 }
 jansNotation([5, 3, 4, "*", "-"]);
