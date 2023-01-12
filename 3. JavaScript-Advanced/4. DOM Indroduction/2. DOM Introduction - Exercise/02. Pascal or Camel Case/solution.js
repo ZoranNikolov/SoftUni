e025ba23960e6fb337f1f944269f97b2c6c0d6b3
Zoran Nikolov <zoran902@gmail.com>
2023-01-12 15:34:45 +0200

    update

--- 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+	let text = document.getElementById("text").value;
+	let convention = document.getElementById("naming-convention").value;
+	let textArray = text.split(" ");
+	let res = "";
+
+	switch (convention) {
+		case "Camel Case":
+			textArray.forEach((el, i) => {
+				i === 0
+					? (res += el.toLowerCase())
+					: (res += el[0].toUpperCase() + el.substring(1).toLowerCase());
+			});
+			break;
+		case "Pascal Case":
+			textArray.forEach((el) => {
+				res += el[0].toUpperCase() + el.substring(1).toLowerCase();
+			});
+			break;
+		default:
+			res = "Error!";
+	}
+	document.getElementById("result").textContent = res;
 }
