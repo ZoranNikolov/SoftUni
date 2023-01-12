687a95c74fc38666fabff890153fbc99abdc65f5
Zoran Nikolov <zoran902@gmail.com>
2023-01-12 17:00:54 +0200

    update

--- 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/06. Format the Text/solution.js
+++ 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+	let input = document.getElementById("input").value;
+	let arrayText = input.split(". ");
+	let buff = "";
+
+	for (let i = 0; i < arrayText.length; i += 3) {
+		let res = [];
+		for (let j = 0; j < 3; j++) {
+			if (arrayText[i + j]) {
+				res.push(arrayText[i + j]);
+			}
+		}
+		let resText = res.join(". ") + ".".trim();
+	}
 }
