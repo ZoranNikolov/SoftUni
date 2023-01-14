e18f6331057b56f018ce0460bd80de31d21c3be2
Zoran Nikolov <zoran902@gmail.com>
2023-01-14 21:44:34 +0200

    update

--- 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/08. Generate Report/report.js
+++ 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,32 @@
 function generateReport() {
-    //TODO
+	let tableRow = document.querySelectorAll("tbody tr");
+	let tableHeader = document.querySelectorAll("thead tr th");
+	let res = [];
+
+	//rows
+	for (let i = 0; i < tableRow.length; i++) {
+		let tableData = tableRow[i].children;
+		let tempData = {};
+
+		//columns
+		for (let index = 0; index < tableData.length; index++) {
+			let infoTheader = tableHeader[index].childNodes;
+
+			//check is the checkbox is checked
+			if (infoTheader[1].checked === true) {
+				//the key is the name of text content of th checkbox
+				let key = infoTheader[0].textContent.trim().toLocaleLowerCase();
+
+				//the value is the text content of the current cell in the column
+				let value = tableData[index].textContent;
+
+				//create an object (key - value pair)
+				tempData[key] = value;
+
+				//store the object
+				res.push(tempData);
+			}
+		}
+	}
+	document.getElementById("output").textContent = JSON.stringify(res);
 }
