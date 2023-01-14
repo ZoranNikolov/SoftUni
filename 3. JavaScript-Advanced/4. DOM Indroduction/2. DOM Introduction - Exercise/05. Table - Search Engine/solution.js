4f149e22b18467115cba0e62ba842fd45367e732
Zoran Nikolov <zoran902@gmail.com>
2023-01-14 13:10:10 +0200

    update

--- 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,29 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+	document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+	function onClick() {
+		let input = document.getElementById("searchField").value;
+		let table = document.getElementsByTagName("tr");
 
-   }
+		let isMatched = false;
+
+		for (let i = 2; i < table.length; i++) {
+			let row = table[i];
+			let tempArr = [];
+
+			Array.from(row.children).forEach((cell) => {
+				if (
+					cell.textContent.toLowerCase().includes(input.toLowerCase())
+				) {
+					console.log("Hurray!");
+					isMatched = true;
+				}
+			});
+
+			if (isMatched) {
+				break;
+			}
+		}
+		// debugger;
+	}
 }
