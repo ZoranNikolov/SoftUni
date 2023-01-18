b36aabb519c182072ea94560ac5a996781199558
Zoran Nikolov <zoran902@gmail.com>
2023-01-18 16:01:34 +0200

    update

--- 3. JavaScript-Advanced/5. DOM Manipulations and events/1. DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ 3. JavaScript-Advanced/5. DOM Manipulations and events/1. DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+	let newElemenet = document.getElementById("newItemText").value;
+	let list = document.getElementById("items");
+
+	if (newElemenet.length === 0) {
+		return;
+	}
+
+	let listItem = document.createElement("li");
+	listItem.textContent = newElemenet;
+	list.appendChild(listItem);
+
+	let remove = document.createElement("a");
+	let linkText = document.createTextNode("[Delete]");
+	remove.appendChild(linkText);
+	remove.href = "#";
+	remove.addEventListener("click", deleteItem);
+
+	listItem.appendChild(remove);
+
+	function deleteItem() {
+		listItem.remove();
+	}
 }
