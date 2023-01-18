591a9ee8f9631ddcdc1efed4879ed6b22c65af46
Zoran Nikolov <zoran902@gmail.com>
2023-01-18 14:56:28 +0200

    updated

--- 3. JavaScript-Advanced/5. DOM Manipulations and events/1. DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++ 3. JavaScript-Advanced/5. DOM Manipulations and events/1. DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+	let input = document.getElementById("newItemText").value;
+	if (input != "") {
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode(input));
+		document.getElementById("items").appendChild(li);
+		document.getElementById("newItemText").value = "";
+	}
 }
