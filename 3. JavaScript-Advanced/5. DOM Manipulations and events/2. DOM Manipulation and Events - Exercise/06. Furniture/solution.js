526c40d3cf16ef1b82d6cc652c33adc35d7b132f
Zoran Nikolov <zoran902@gmail.com>
2023-01-19 16:53:51 +0200

    update

--- 3. JavaScript-Advanced/5. DOM Manipulations and events/2. DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ 3. JavaScript-Advanced/5. DOM Manipulations and events/2. DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,4 +1,43 @@
 function solve() {
+	let buttons = document.querySelectorAll("button");
+	buttons[0].addEventListener("click", generate);
+	buttons[1].addEventListener("click", buy);
 
-  //TODO...
+	function generate() {
+		let currentItems = JSON.parse(document.querySelectorAll("textarea")[0].value);
+		let tableBody = document.getElementsByTagName('tbody')[0]
+		for (let item of currentItems) {
+			let tableRow = document.createElement("tr");
+			tableRow.innerHTML = `<td>
+									<img src="${item.img}">	
+								</td>
+								<td>
+									<p>${item.name}</p>
+								</td>
+								<td>
+									<p>${item.price}</p>
+								</td>
+								<td>
+									<p>${item.decFactor}</p>
+								</td>
+								<td>
+									<input type="checkbox">
+								</td>`;
+			tableBody.appendChild(tableRow);
+			// let tableData = document.createElement("td");
+			// let img = document.createElement("img");
+			// img.setAttribute("src", item.img);
+			// tableData.appendChild(img);
+			// tableRow.appendChild(tableData);
+			// let townTableData = document.createElement("td");
+			// let townName = document.createElement("p");
+			// townName.innerText = item.name;
+			// townTableData.appendChild(townName);
+			// tableRow.appendChild(townTableData);
+			// tableBody.appendChild(tableRow);
+			// debugger;
+		}
+	}
+
+	function buy() {}
 }
