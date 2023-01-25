40ae36bce3120dc1aecb36297a25de3feaebf936
Zoran Nikolov <zoran902@gmail.com>
2023-01-25 11:49:03 +0200

    updated

---
+++ 3. JavaScript-Advanced/6. Advanced Functions/2. Advanced Functions - Exercise/04.breakfastRobot.js
@@ -0,0 +1,9 @@
+function solution(){
+
+	
+
+}
+
+let manager = solution (); 
+console.log (manager ("restock flavour 50")); // Success 
+console.log (manager ("prepare lemonade 4")); // Error: not enough carbohydrate in stock 
