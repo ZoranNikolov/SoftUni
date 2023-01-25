84af9a057c2423ce332d5b3b7dad5a76647df996
Zoran Nikolov <zoran902@gmail.com>
2023-01-25 15:07:09 +0200

    updated

--- 3. JavaScript-Advanced/6. Advanced Functions/2. Advanced Functions - Exercise/08. Next Article/script.js
+++ 3. JavaScript-Advanced/6. Advanced Functions/2. Advanced Functions - Exercise/08. Next Article/script.js
@@ -2,7 +2,7 @@ function getArticleGenerator(articles) {
 	let myArticles = Array.from(articles);
 	let content = document.getElementById("content");
 	return () => {
-		if (!myArticles.length) {
+		if (myArticles.length === 0) {
 			return;
 		}
 		let currentArticle = myArticles.shift();
