7cff54bd38053e3bf22de225858245d200434092
Zoran Nikolov <zoran902@gmail.com>
2023-01-26 13:20:27 +0200

    updated  git status clear

--- 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/01. Subtraction/subtract.js
+++ 3. JavaScript-Advanced/4. DOM Indroduction/2. DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -1,7 +1,6 @@
 function subtract() {
 	let firstNumber = Number(document.getElementById("firstNumber").value);
 	let secondNumber = Number(document.getElementById("secondNumber").value);
-
 	let result = firstNumber - secondNumber;
 	document.getElementById("result").innerText = result;
 }
