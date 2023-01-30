c2f271c2ede16c87448508c3da3981918280e759
Zoran Nikolov <zoran902@gmail.com>
2023-01-30 13:44:04 +0200

    updated

--- Other/index.js
+++ Other/index.js
@@ -152,3 +152,43 @@ let okIterator = "OK"[Symbol.iterator]();
 console.log(okIterator.next()); // → {value: "O", done: false}
 console.log(okIterator.next()); // → {value: "K", done: false}
 console.log(okIterator.next()); // → {value: undefined, done: true}
+
+class Matrix {
+	constructor(width, height, element = (x, y) => undefined) {
+		this.width = width;
+		this.height = height;
+		this.content = [];
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				this.content[y * width + x] = element(x, y);
+			}
+		}
+	}
+	get(x, y) {
+		return this.content[y * this.width + x];
+	}
+	set(x, y, value) {
+		this.content[y * this.width + x] = value;
+	}
+}
+class MatrixIterator {
+	constructor(matrix) {
+		this.x = 0;
+		this.y = 0;
+		this.matrix = matrix;
+	}
+	next() {
+		if (this.y == this.matrix.height) return { done: true };
+		let value = {
+			x: this.x,
+			y: this.y,
+			value: this.matrix.get(this.x, this.y),
+		};
+		this.x++;
+		if (this.x == this.matrix.width) {
+			this.x = 0;
+			this.y++;
+		}
+		return { value, done: false };
+	}
+}
