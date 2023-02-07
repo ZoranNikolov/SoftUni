05f488f9bab243fd6f177dc036cb19af0f1a804e
Zoran Nikolov <zoran902@gmail.com>
2023-02-07 11:00:08 +0200

    'updated'

--- 5. JavaScript - Web/JS Back-End/01. Cat-Shelter-Workshop/index.js
+++ 5. JavaScript - Web/JS Back-End/01. Cat-Shelter-Workshop/index.js
@@ -1,28 +1,34 @@
 const fs = require("fs/promises");
+const fss = require("fs");
 const http = require("http");
-const path = require("path");
+const qs = require("querystring");
 
 const cats = require("./cats.json");
 
 const server = http.createServer(async (req, res) => {
+	console.log(req.url);
 	res.writeHead(200, {
 		"Content-Type": "text/html",
 	});
 
 	if (req.url == "/") {
-		const homePage = await fs.readFile("./views/home.html");
-		
-		res.write(homePage);
+		const homePage = await readFile("./views/home.html");
+		const catsHtml = cats.map((cat) => catTemplate(cat)).join("");
+		const result = homePage.replace("{{cats}}", catsHtml);
+		res.write(result);
 	} else if (/cats\/\d+\/edit/.test(req.url)) {
-		let catId = req.url.split("/")[2];
-		let cat = cats.find((x) => x.id == catId);
-		// res.write(editCat(cat));
+		// let catId = req.url.split("/")[2];
+		// let cat = cats.find((x) => x.id == catId);
+		let editCatHtml = await readFile("./views/editCat.html");
+		let qsData = qs.parse(req.url.split('?')[1])
+		console.log(qsData);
+		res.write(editCatHtml);
 	} else if (req.url == "/css/site.css") {
 		res.writeHead(200, {
 			"Content-Type": "text/css",
 		});
 
-		const siteCss = readFile('./content/styles/site.css')
+		const siteCss = readFile("./content/styles/site.css");
 		res.write(siteCss);
 	} else {
 		res.write(`
@@ -33,12 +39,25 @@ const server = http.createServer(async (req, res) => {
 	res.end();
 });
 
-function readFile(path){
-return fs.readFile(path, { encoding: 'utf-8' })
+function readFile(path) {
+	return fs.readFile(path, { encoding: "utf-8" });
 }
 
-async function catTemplate(cat){
-	const html = await fs.readFile
+function catTemplate(cat) {
+	const html = fss.readFileSync("./views/partials/cat.html", {
+		encoding: "utf-8",
+	});
+
+	// let result = html.replaceAll("{{name}}", cat.name);
+	// result = result.replace("{{description}}", cat.description);
+	// result = result.replace("{{imageUrl}}", cat.imageUrl);
+	// result = result.replace("{{breed}}", cat.breed);
+
+	const modifiedHtml = Object.keys(cat).reduce((result, key) => {
+		return result.replaceAll(`{{${key}}}`, cat[key]);
+	}, html);
+
+	return modifiedHtml;
 }
 
 server.listen(5000);
